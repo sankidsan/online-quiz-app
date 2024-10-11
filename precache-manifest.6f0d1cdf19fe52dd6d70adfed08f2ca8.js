844cb1a459d9e899b40326dec8d9c8a6f872086f
santhoshgit <santhoshkumar240503@gmail.com>
2024-10-11 16:30:52 +0530

    Updates

---
+++ precache-manifest.6f0d1cdf19fe52dd6d70adfed08f2ca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7983d99d539e285e6abe1d75303c0ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5db09245b843e1c5e8e",
+    "url": "/static/css/main.bdc17925.chunk.css"
+  },
+  {
+    "revision": "bdf77258b329c5f62515",
+    "url": "/static/js/2.e9fd7691.chunk.js"
+  },
+  {
+    "revision": "e5db09245b843e1c5e8e",
+    "url": "/static/js/main.c93ffd8a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
