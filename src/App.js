a980e33ec17736f5470baaa10ab690b588c51d66
Santhosh Kumar <santhoshkumar240503@gmail.com>
2024-10-11 14:33:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct import for BrowserRouter
 import CreateQuiz from './components/CreateQuiz';
 import TakeQuiz from './components/TakeQuiz';
 import Result from './components/Result';
@@ -11,16 +10,25 @@ function App() {
   const [score, setScore] = useState(0);
 
   return (
-   
-    <Router basename="/online-quiz-app">
+    <Router basename="/online-quiz-app/">
       <div className="App">
         <header className="header">
           <h1>Online Quiz Platform</h1>
         </header>
         <Routes>
-          <Route path="/" element={<CreateQuiz setQuizData={setQuizData} />} />
-          <Route path="/take-quiz" element={<TakeQuiz quizData={quizData} setScore={setScore} />} />
-          <Route path="/result" element={<Result score={score} totalQuestions={quizData.length} />} />
+          <Route
+            path="/"
+            element={<CreateQuiz setQuizData={setQuizData} />}
+            exact
+          />
+          <Route
+            path="/take-quiz"
+            element={<TakeQuiz quizData={quizData} setScore={setScore} />}
+          />
+          <Route
+            path="/result"
+            element={<Result score={score} totalQuestions={quizData.length} />}
+          />
         </Routes>
       </div>
     </Router>
